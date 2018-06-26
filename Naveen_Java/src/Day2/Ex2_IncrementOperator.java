f46c7f86dd94b80f52b9e168e4c9d1850dabb9fd
Madhu X1Tab <madhu4mobile@gmail.com>
2018-06-25 23:00:50 -0400

    After Week2 Day1 Functions class

--- Naveen_Java/src/Day2/Ex2_IncrementOperator.java
+++ Naveen_Java/src/Day2/Ex2_IncrementOperator.java
@@ -1,4 +1,4 @@
-package Day2;
+package Day4;
 
 public class Ex2_IncrementOperator {
 
@@ -9,24 +9,24 @@ public class Ex2_IncrementOperator {
 		y2=3;
 		z2=4;
 		//System.out.println(""+z2+y2++);
-		
+
 		System.out.println(""+z2+y2++*2);// ans is 46
-		
-		
+
+
 		// reson is simple
 		// in the case of (""+z2+y2++*2)  --> from left to right, because of "", null string is added to z2. so it will be a string 4.
 		// then while doing y2++, first the initial value of y2 = 3 is concatenated to 4 and the result is 43.
 		//after that,y2 value is increased by 1 but it will be stored as 4 in memory. For the present line, the value of y2 is still 3
 		// due to y2*x2, the last digit is printed as 6.
-		
+
 		// proof if we print y2 value in the next line, we can see y2 as 4.
-		
+
 		System.out.println(y2);//y2 is 4 now as the value of y2 is increased in last step.
-		// 
+		//
 		//now we can expalin why the answer is 12 for the follwing print.
 		System.out.println(z2+y2++*x2);// ans is 12   Why is that??
-		
-		
+
+
 	}
 
 }
---
+++ Naveen_Java/src/Week2_Day1/OOPs_Functions.java
@@ -0,0 +1,63 @@
+package Week2_Day1;
+
+public class OOPs_Functions {
+
+	public static void main(String[] args) {
+		// functions and Methods are same
+		// function or method is like a factory. It produces something.
+		// There are three type of factories
+		//1. Main is also a method. There is a rule. We can not create a method inside a method.
+		
+		///// ****** Calling methods from Main
+		// an object has to be created.
+		
+		// structure to initiate an object in Java ---> ClassName Object_Ref_Variable = new ClassName()
+		OOPs_Functions ob = new OOPs_Functions();
+		
+		ob.
+		
+		
+		
+		
+		
+	}
+	//Rules: 1. Can not create a function inside a function
+	//2. function can not return multiple vaalues. No multiple return statements
+	//3. functions are independent -- parallel to each other.
+	//4. no duplicate functions are allowed.
+	//5. but a method can be over loaded. A function with same name but different parameters.
+	//6. 
+	
+	
+	//1. No input and No output - Method.  
+	// This function doesnot accepts anything and does not returns anyting
+	// void -- it does not return any value:
+	public void test() {
+		System.out.println("Method with no return");
+	}
+	//2. No input but some output - Method
+	// this does not take any input but returns something
+	public int sum() {
+		System.out.println("This method takes nothing but gives sum");
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		return c; // when there is a return the datatype can not be void. It should proper data type like int here.
+	}
+	//2a. some function to return a string
+	public String getName() {
+		System.out.println("This method should return a string");
+		return "Tom";
+	}
+	//3. Some input and some output method 
+	// this should take some values and returns some values
+	public int div(int a, int b) {
+		System.out.println("This method takes integers a and b and returns ");
+		int d = int a/b;
+		return d;
+	}
+	
+	
+	
+	
+}
